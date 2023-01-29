3baa7ee6bd28f8ac2911a66d7f789c97f0c4dc9b
Hoon Yim <yimsh4507@gmail.com>
2023-01-29 07:16:01 -0500

    Updated app.js - added a global exception handler and uncaught error handler

--- app.js
+++ app.js
@@ -12,4 +12,14 @@ app.use(express.json());
 const userRouter = require("./routes/userRoutes");
 app.use("/api/users", userRouter);
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+
+// Exception Handler for uncaught errors
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server..`, 404));
+});
+// Global Exception Handler
+app.use(globalErrorHandler);
+
 module.exports = app;

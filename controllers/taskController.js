de5b8892021125c6ae1957e5e9f83fe0e60fec20
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-02-10 16:13:07 -0500

    added API's for tasks

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -3,6 +3,7 @@ const Task = require("../models/taskModel");
 const AppError = require("../utils/appError");
 const catchAsync = require("../utils/catchAsync");
 
+// Display all tasks
 exports.getAllTasks = catchAsync(async (req, res) => {
     const tasks = await Task.find();
 
@@ -10,5 +11,30 @@ exports.getAllTasks = catchAsync(async (req, res) => {
         status: "success",
         results: tasks.length,
         tasks
+    });
+})
+
+
+// Create new task
+exports.createTask = catchAsync(async (req, res) => {
+    const task = new Task(req.body);
+    await task.save();
+
+    res.status(500).json({
+        status: "success",
+        results: task.length,
+        task
+    });
+})
+
+// Display task by id
+exports.getTaskById = catchAsync(async (req, res) => {
+    const taskId = req.params.tid
+    const task = await Task.find({ _id: taskId })
+
+    res.status(500).json({
+        status: "success",
+        results: task.length,
+        task
     });
 })

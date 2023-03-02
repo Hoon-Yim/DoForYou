92febb4eb24f7a4b7ff161d0121aa46bc79b58b1
Hoon Yim <yimsh4507@gmail.com>
2023-03-02 09:24:35 -0500

    Updated Chat.js - testing purpose

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import Cookies from "universal-cookie";
 import { useState, useEffect } from "react";
 import io from "socket.io-client";
@@ -9,36 +10,77 @@ function Chat() {
     const [firstname, setFirstname] = useState("");
     const [text, setText] = useState("");
     const [messages, setMessages] = useState([]);
+    const [room, setRoom] = useState("");
+    const [rooms, setRooms] = useState([]);
+
+    const[isJoined, setIsJoined] = useState(false);
 
     // getting firstname for sending message
     useEffect(() => {
         setFirstname(cookies.get("firstname"));
+        
+        axios.get("http://localhost:8000/api/chat/rooms")
+            .then(data => {
+                setRooms(data.data.rooms);
+            });
     }, []);
 
-
+    useEffect(() => {
+        socket.on("send_previous_messages", messages => {
+            setMessages(messages.map(message => {
+                return `${message.sender.firstname}: ${message.message}`
+            }))
+        })
+    }, [isJoined]);
 
     // getting messages sent by other users
     useEffect(() => {
-        socket.on("message", msg => {
+        socket.on("retrieve_message", msg => {
             console.log(msg);
             setMessages([...messages, msg]);
         });
-    }, [messages]);
+    }, [messages, socket]);
     
     return (
         <>
-            <form onSubmit={e => {
-                e.preventDefault();
-                socket.emit("message", { firstname, message: text });
-                setText("");
-            }}>
-                <input type="text" onChange={e => {
-                    setText(e.target.value);
-                }} value={text} />
-            </form>
-
             {
-                messages.map(message => <><span>{message}</span><br/></>)
+                isJoined ?
+                <>
+                    <form onSubmit={e => {
+                        e.preventDefault();
+                        socket.emit("send_message", { roomId: room, firstname, message: text });
+                        setMessages([...messages, `${firstname}: ${text}`]);
+                        setText("");
+                    }}>
+                        <input type="text" onChange={e => {
+                            setText(e.target.value);
+                        }} value={text} />
+                    </form>
+                    {
+                        messages.map(message => <><span>{message}</span><br /></>)
+                    }
+                </>
+                :
+                <>
+                    <button onClick={() => {
+                        axios.post("http://localhost:8000/api/chat/rooms")
+                            .then(data => {
+                                console.log(data.data);
+                            });
+                    }}>Create</button>
+                    {rooms &&
+                        rooms.map(room => <><span>{room._id}</span><br /></>)
+                    }
+                    <form onSubmit={e => {
+                        e.preventDefault();
+                        socket.emit("join_room", room);
+                        setIsJoined(true);
+                    }}>
+                        <input type="text" onChange={e => {
+                            setRoom(e.target.value);
+                        }} value={room} />
+                    </form>
+                </>
             }
         </>
     )

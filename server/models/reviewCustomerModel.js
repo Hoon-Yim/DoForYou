c9f1a86435bce9e5624f7445ae6574100d7e8df4
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-03-03 13:07:35 -0500

    added count to tags

--- server/models/reviewCustomerModel.js
+++ server/models/reviewCustomerModel.js
@@ -16,32 +16,72 @@ const reviewCustomerSchema = mongoose.Schema({
         count: {
             type: Number,
         },
-        tags:{
-            type: String,
-            enum: [
-                "Punctual payment",
-                "Good manners",
-                "Friendly",
-                "Great Communication",
-                "Reasonable request",
-                "Positive"
-            ]
+        tags: {
+            selectedTag: {
+                type: String,
+                enum: [
+                    "Punctual payment",
+                    "Good manners",
+                    "Friendly",
+                    "Great Communication",
+                    "Reasonable request",
+                    "Positive"
+                ]
+            },
+            countTag1: {
+                type: Number
+            },
+            countTag2: {
+                type: Number
+            },
+            countTag3: {
+                type: Number
+            },
+            countTag4: {
+                type: Number
+            },
+            countTag5: {
+                type: Number
+            },
+            countTag6: {
+                type: Number
+            }
         }
     },
     dislikes: {
         count: {
             type: Number
         },
-        tags:{
-            type: String,
-            enum: [
-                "Payment delay",
-                "Lack of description",
-                "Rude",
-                "Poor Communication",
-                "Unreasonable request",
-                "Negative"
-            ]
+        tags: {
+            selectedTag: {
+                type: String,
+                enum: [
+                    "Payment delay",
+                    "Lack of description",
+                    "Rude",
+                    "Poor Communication",
+                    "Unreasonable request",
+                    "Negative"
+                ]
+            },
+            countTag1: {
+                type: Number
+            },
+            countTag2: {
+                type: Number
+            },
+            countTag3: {
+                type: Number
+            },
+            countTag4: {
+                type: Number
+            },
+            countTag5: {
+                type: Number
+            },
+            countTag6: {
+                type: Number
+            }
         }
     }
 });

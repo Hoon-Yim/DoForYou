ad3b7c27c2e63c5f953c485317eac1e3265c8e81
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-03-08 20:57:02 -0500

    added new routes

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 const reviewController = require("../controllers/reviewController")
 
-//router.post("/create", Controller.createTask);
-//router.get("/:tid", taskController.getTaskById);
+router.post("/reviewCustomer/:uid", reviewController.createCustomerReview);
+router.post("/reviewPerformer/:uid", reviewController.createPerformerReview);
+router.get("/:uid", reviewController.getUserReviews);
 
 router
     .route('/')
-    //.get(reviewController.getAllTasks);
+    .get(reviewController.getPerformersReviews);
 
 module.exports = router;

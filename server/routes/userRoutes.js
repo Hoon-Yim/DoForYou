8527a72d37fe675c11f20d1d5985334dbca20f62
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-03-30 19:49:57 -0400

    added upload picture feature

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const upload = require("../controllers/uploadController");
 
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
@@ -8,7 +9,9 @@ router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
-
+router.post('/profile/:uid', upload.single('photo'), authController.uploadPicture);
+router.get("/profile/:uid", authController.getUserPicture);
+router.put("/profile/:uid", upload.single('photo'), authController.updateUserPicture);
 router
     .route('/')
     .get(userController.getAllUsers);

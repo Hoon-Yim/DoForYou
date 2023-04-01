ed03e6d7b187c8ad0563f8f46f0af7381bfcdac7
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-04-01 00:18:58 -0400

    added file upload feature

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../controllers/uploadController");
+const upload = require("../controllers/uploadImageController");
+const uploadFile = require("../controllers/uploadFileController");
 
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
@@ -11,6 +12,10 @@ router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
 router.post('/profile/:uid', upload.single('photo'), userController.uploadPicture);
 router.get("/profile/:uid", userController.getUserPicture);
+router.post("/profile/uploadFile/:uid", uploadFile.single('file'), userController.uploadFile);
+router.get("/profile/files/:uid", userController.getUserFiles);
+router.put("/profile/files/:uid", userController.deleteAllFiles);
+
 router
     .route('/')
     .get(userController.getAllUsers);

330ce20cfe8b14614140364acae15b44530d575a
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-04-02 16:59:16 -0400

    added admin panel

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-//import View from './components/View'
+import View from './components/View'
 import Login from './components/Login'
 //import Update from './components/Update'
-//import ViewEmployee from './components/ViewEmployee';
+import ViewFiles from './components/ViewFiles';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="main">
-    <div>
+    <div className="main ">
+    <div className="bg-primary">
     <Router>
       <Routes>
         <Route path="/" element={<Login/>}/>
+        <Route path="/view" element={<View/>}/>
+        <Route path="/viewFiles" element={<ViewFiles/>}/>
       </Routes>
     </Router>
     </div>

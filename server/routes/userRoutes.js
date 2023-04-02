9517aa31c948859dcb871209a40744618370330c
Elizaveta Vygovskaia <vygovskaia.e@gmail.com>
2023-04-02 16:58:55 -0400

    added verification

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../controllers/uploadImageController");
-const uploadFile = require("../controllers/uploadFileController");
 
 const userController = require("../controllers/userController");
 const authController = require("../controllers/authController");
@@ -12,12 +11,8 @@ router.post("/forgotPassword", authController.forgotPassword);
 router.patch("/resetPassword/:token", authController.resetPassword);
 router.post('/profile/:uid', upload.single('photo'), userController.uploadPicture);
 router.get("/profile/:uid", userController.getUserPicture);
-router.post("/profile/uploadFile/:uid", uploadFile.single('file'), userController.uploadFile);
-router.get("/profile/files/:uid", userController.getUserFiles);
-router.get("/profile/files/:uid/:fid", userController.getFileById);
-router.put("/profile/files/:uid", userController.deleteAllFiles);
 router.get("/notVerified", userController.getNotVerifiedUsers);
-router.put("/verified", userController.verifyUser);
+router.put("/verify/:uid", userController.verifyUser);
 
 router
     .route('/')
